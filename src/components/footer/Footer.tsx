ad1c2706cf612cb00bbb54056efae1b8942a61a6
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-02-13 03:43:53 -0700

    feat: remove empty container from footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,28 +1,24 @@
 import type { FC } from 'react';
 
-import Container from '@/components/container/Container';
 import LogoLink from '@/components/link/Logo';
 import Socials from '@/components/socials/Socials';
 import { discordNames } from '@/utils/types';
 
 const Footer: FC = () => (
-  <>
-    <Container className="flex grow flex-col" />
-    <footer className="flex flex-col place-items-center content-center justify-center gap-2 p-4 text-center">
-      <div className="flex flex-col xs:flex-row justify-around gap-2 xs:gap-16 items-center">
-        <LogoLink />
-        <Socials />
-      </div>
-      <span>Credits to: The Yaphalla Community</span>
-      <div>
-        <span>Developed by: </span>
-        <i className="fab fa-discord" /> <span>{discordNames.map(name => name)}</span>
-      </div>
-      <p className="text-sm md:text-base">
-        Yaphalla is not endorsed or affiliated with Lilith Games, or any of its subsidaries/affiliates
-      </p>
-    </footer>
-  </>
+  <footer className="flex flex-col place-items-center content-center justify-center gap-2 p-4 text-center">
+    <div className="flex flex-col xs:flex-row justify-around gap-2 xs:gap-16 items-center">
+      <LogoLink />
+      <Socials />
+    </div>
+    <span>Credits to: The Yaphalla Community</span>
+    <div>
+      <span>Developed by: </span>
+      <i className="fab fa-discord" /> <span>{discordNames.map(name => name)}</span>
+    </div>
+    <p className="text-sm md:text-base">
+      Yaphalla is not endorsed or affiliated with Lilith Games, or any of its subsidaries/affiliates
+    </p>
+  </footer>
 );
 
 export default Footer;

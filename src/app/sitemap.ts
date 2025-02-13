19074df539574da723437157a6a500ca44a58af4
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-02-13 05:14:42 -0700

    fix: fix sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const url = `https://${domain}`;
 
   const pathData = Object.values(paths).map(({ href }) => ({
-    url: `${url}/${href}`,
+    url: `${url}${href}`,
     lastModified: new Date(),
     priority: 1,
   }));

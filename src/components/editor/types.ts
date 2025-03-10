995aa655c629456c38e4a798563196eda5b18715
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-03-10 05:12:07 -0600

    feat: add new constants for tile state logic

--- src/components/editor/types.ts
+++ src/components/editor/types.ts
@@ -221,7 +221,7 @@ export const TileLayout = [
 ] as const;
 
 export type TileData = {
-  state: -1 | 0 | 1;
+  state: number;
   index: number;
 };
 
@@ -236,6 +236,14 @@ export type TileDivData = {
   reverse?: string;
   preview?: string;
 };
+
+/*
+   -1: enemy
+    0: empty
+    1: player
+    2: artifact
+  100: disable
+*/
 
 export const ArenaPresets = {
   Custom: Array(45).fill(0) as number[],
@@ -286,6 +294,11 @@ export const ArenaPresets = {
   ],
 } as const;
 
+export const DoubleTiles = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 2, 1, 1, 1, 1, 0, 2,
+  100, 1, 1, 0, 0, 0,
+] as const;
+
 export const indexToPosition = [
   43, 45, 35, 38, 40, 42, 44, 28, 31, 34, 37, 39, 41, 21, 24, 27, 30, 33, 36, 14, 17, 20, 23, 26, 29, 32, 10, 13, 16,
   19, 22, 25, 5, 7, 9, 12, 15, 18, 2, 4, 6, 8, 11, 1, 3,
@@ -311,3 +324,5 @@ export const TalentLocations = {
 } as const;
 
 export const LogoRegExp = new RegExp('Cat|Dog');
+
+export const UnitState = new Set([1, 2, 100]);

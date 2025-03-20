8c9310afb554fa7ccee99b812d7e688a8060c56d
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-03-20 03:58:25 -0600

    feat: extract 404 content to NotFound component

---
+++ src/components/not-found/NotFound.tsx
@@ -0,0 +1,19 @@
+import type { FC } from 'react';
+
+import Container from '@/components/container/Container';
+import Link from '@/components/link/Link';
+
+const NotFound: FC = () => (
+  <Container className="mt-0 mb-18 flex grow size-full max-w-full flex-col items-center justify-center self-center align-middle">
+    <div className="flex flex-col bg-primary-950/80 gap-4 rounded-lg p-4 justify-center items-center">
+      <h1>
+        <span className="text-red-700">404</span> | <span className="text-red-700">Not Found</span>
+      </h1>
+      <Link href="/" className="size-base input-secondary">
+        Return Home
+      </Link>
+    </div>
+  </Container>
+);
+
+export default NotFound;

c6ed383c693767d6f69ba6821341a07eea1df6b5
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-03-24 21:30:23 -0600

    feat: add tooltip component

---
+++ src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,24 @@
+import type { FC, PropsWithChildren } from 'react';
+
+import { joinStrings } from '@/utils/utils';
+
+interface TooltipProps extends PropsWithChildren {
+  className?: string;
+  pointerEvents?: boolean;
+  preWrapText?: boolean;
+}
+
+const Tooltip: FC<TooltipProps> = ({ className, pointerEvents = false, preWrapText = true, children }) => (
+  <div
+    className={joinStrings(
+      'container-primary !p-1 hidden absolute w-fit z-10 !text-white group-hover:block',
+      preWrapText && 'whitespace-pre',
+      !pointerEvents && 'pointer-events-none',
+      className,
+    )}
+  >
+    {children}
+  </div>
+);
+
+export default Tooltip;

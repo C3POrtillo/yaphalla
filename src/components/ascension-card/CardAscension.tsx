c077e45ae0a4271b3f0edba2ab5f033b8ca610eb
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-07-24 05:52:10 -0400

    feat: add ascension card

---
+++ src/components/ascension-card/CardAscension.tsx
@@ -0,0 +1,103 @@
+import * as htmlToImage from 'html-to-image';
+
+import type { StyleTypes } from '@/components/ascension-card/types';
+import type { FC } from 'react';
+
+import AscensionHex from '@/components/ascension-card/AscensionHex';
+import AscensionPortrait from '@/components/ascension-card/AscensionPortrait';
+import { useHeroData } from '@/components/ascension-card/HeroDataProvider';
+import IconAscension from '@/components/ascension-card/IconAscension';
+import IconExWeapon from '@/components/ascension-card/IconExWeapon';
+import { AscensionCardType, ExWeapon } from '@/components/ascension-card/types';
+import ExportImage from '@/components/export-image/ExportImage';
+import HexImage from '@/components/hex-tiles/HexImage';
+import Dropdown from '@/components/inputs/dropdown/Dropdown';
+import { Ascension } from '@/utils/types';
+import { compareStrings, joinStrings } from '@/utils/utils';
+
+export interface CardAscensionProps {
+  styleType?: StyleTypes;
+  cardClassName?: string | boolean;
+}
+
+const CardAscension: FC<CardAscensionProps> = ({ styleType = 'container', cardClassName }) => {
+  const { hero, type, setType, ascension, setAscension, exWeapon, setExWeapon, isExport, setExport, exportId } =
+    useHeroData();
+  const dropdowns = [
+    {
+      label: `Layout: ${type}`,
+      options: AscensionCardType,
+      callback: (option: string | number) => setType(option as AscensionCardType),
+    } as const,
+    {
+      label: (
+        <span className="flex flex-row items-center gap-2">
+          <span className="inline-flex flex-row align-middle">
+            <IconAscension src={ascension} size="w-3" />
+          </span>
+          {ascension}
+        </span>
+      ),
+      options: Ascension,
+      optionIcons: Ascension.map((src, i) => <IconAscension key={`${src}-${i}`} src={src} size="w-3" />),
+      callback: (option: string | number) => setAscension(option as Ascension),
+    } as const,
+    {
+      label: (
+        <span className="flex flex-row items-center gap-2">
+          <span className="min-w-7 text-right">{exWeapon}</span>
+          <span className="inline-flex flex-row align-middle">
+            <IconExWeapon src={exWeapon} type={type} size="h-6" />
+          </span>
+        </span>
+      ),
+      options: ExWeapon,
+      optionIcons: ExWeapon.map((src, i) => <IconExWeapon key={`${src}-${i}`} src={src} type={type} size="h-6" />),
+      callback: (option: string | number) => setExWeapon(option as ExWeapon),
+      optionIconPosition: 'right',
+    } as const,
+  ];
+
+  return (
+    <div
+      className={joinStrings(`${styleType}-primary`, 'flex flex-col gap-1 justify-start items-center', cardClassName)}
+    >
+      <span className="flex flex-row items-center gap-2 border-b-2 border-tertiary-600 w-full items-center pb-2 mb-1">
+        <span className="inline-flex flex-row align-middle">
+          <HexImage src={hero} path="unit" size="xs" disabled />
+        </span>
+        <span className="text-lg">{hero}</span>
+      </span>
+      <div className="flex flex-row gap-2 items-center grow">
+        <div className="flex flex-col gap-1 min-w-40">
+          {dropdowns.map(({ ...props }, i) => (
+            <Dropdown key={i} hierarchy="secondary" {...props} />
+          ))}
+        </div>
+        {compareStrings(type, 'Hex') ? <AscensionPortrait /> : <AscensionHex />}
+      </div>
+      <div className="w-full pt-1">
+        <ExportImage
+          selected={isExport}
+          getImage={async () => {
+            setExport(true);
+            const ascensionDiv = document.getElementById(exportId);
+            if (!ascensionDiv) {
+              return false;
+            }
+            const image = await htmlToImage.toPng(ascensionDiv, { pixelRatio: 1 });
+
+            return image;
+          }}
+          onClick={() => {
+            setExport(false);
+          }}
+          size="sm"
+          hasContainer={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CardAscension;

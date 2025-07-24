9522988331df15aee4bcb4e1b7dedea4ec5520f9
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-07-24 05:51:09 -0400

    feat: update ascension types

--- src/components/ascension-card/types.ts
+++ src/components/ascension-card/types.ts
@@ -1,2 +1,13 @@
-export type AscensionCardType = 'Portrait' | 'Hex';
-export type ExWeapon = 0 | 5 | 10 | 15 | 20 | 25;
+export const AscensionCardType = ['Hex', 'Card'] as const;
+export type AscensionCardType = (typeof AscensionCardType)[number];
+export const ExWeapon = ['+0', '+5', '+10', '+15', '+20', '+25'] as const;
+export type ExWeapon = (typeof ExWeapon)[number];
+export type StyleTypes = 'container' | 'inset';
+export const FrameSet = new Set([
+  'supreme+',
+  ...(Array(4)
+    .fill(null)
+    .map((_, i) => `paragon ${i + 1}`) as `paragon ${number}`[]),
+  'crown',
+] as const);
+export type FrameSet = typeof FrameSet extends Set<infer T> ? T : never;

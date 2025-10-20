072ff7f45dfe5462e3868cc5743cc59ce2a4104a
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2025-10-20 17:32:47 -0400

    fix: fix spacing

--- src/components/link/Logo.tsx
+++ src/components/link/Logo.tsx
@@ -6,13 +6,13 @@ import Link from '@/components/link/Link';
 
 interface LogoLinkProps {
   alt?: string;
-  }
+}
 
-  const LogoLink: FC<LogoLinkProps> = ({ alt }) => (
-    <Link href="/" className="relative logo input-tertiary p-1 rounded-lg">
-        <Image src="/assets/images/yaphalla-logo.png" alt={alt || 'Homepage'} fill priority />
-          </Link>
-          );
+const LogoLink: FC<LogoLinkProps> = ({ alt }) => (
+  <Link href="/" className="relative logo input-tertiary p-1 rounded-lg">
+    <Image src="/assets/images/yaphalla-logo.png" alt={alt || 'Homepage'} fill priority />
+  </Link>
+);
 
-          export default LogoLink;
+export default LogoLink;
           

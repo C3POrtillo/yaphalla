df5460fbd2c588ba32dd9b9d6a953faed12ca707
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2026-01-29 18:35:17 -0500

    chore: double quote

--- src/components/formation/FormationEditor.tsx
+++ src/components/formation/FormationEditor.tsx
@@ -13,7 +13,7 @@ const FormationEditor: FC = () => {
   return (
     id === currentId && (
       <>
-        <Container className='max-w-[1920px]'>
+        <Container className="max-w-[1920px]">
           <EditorMain />
         </Container>
         <Container className="hidden 2xl:flex">

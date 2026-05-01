5602315aa417fce6a0002fe208dc690146ef2c2d
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2026-05-01 12:15:04 -0400

    fix: fix missing icon

--- src/components/header/utils.ts
+++ src/components/header/utils.ts
@@ -24,7 +24,7 @@ export const getLgCols = (length: number) => {
 };
 
 const discordSlugs = /^\/emotes/;
-const sheetSlugs = new Set(['/primal-lord', '/battle-drills', 'Leaderboards', '/hs-sheets']);
+const sheetSlugs = new Set(['/primal-lord', '/battle-drills', 'Leaderboards', '/hs-sheets', '/tracker']);
 const formSlugs = new Set(['/paragon-form']);
 
 const staticSlugs: Record<string, string> = {

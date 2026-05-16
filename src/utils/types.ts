e5d28685efa82736c5318b991182f4b06a375b78
Camilo III Ortillo <35697764+C3POrtillo@users.noreply.github.com>
2026-05-15 20:12:06 -0400

    fix: fix typo

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -213,7 +213,7 @@ const Mauler = {
 const Graveborn = {
   Tank: ['Callan', 'Daimon', 'Thoran'],
   Support: ['Isabella', 'Ludovic', 'Niru'],
-  Marksman: ['Bonnie', 'Cecia', 'Neron'],
+  Marksman: ['Bonnie', 'Cecia', 'Nerion'],
   Mage: ['Carolina', 'Shemira', 'Viperian'],
   Rogue: ['Nara', 'Salazer', 'Silvina', 'Shadewing'],
   Warrior: ['Hodgkin', 'Igor', 'Valka', 'Zorya'],
